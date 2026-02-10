0f94df93736485199376bfb9074d43d6a59444a2
gusmendez3249 <gustavoangelc2005@email.com>
2026-02-09 19:45:23 -0600

    Primer Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Ejercicio Práctico Unidad 1</h1>
+        <p>Desafío 3 y Desafío 5</p>
+      </header>
+      <main>
+        <p>Aquí irán los componentes...</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
